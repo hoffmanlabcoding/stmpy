8ed64f5a5cbbc63ad9b64bc049168de922a2a2ef
Richard Liu <hoffmanlabcoding@gmail.com>
2023-06-12 16:11:31 -0400

    Update to stmpy 1.0.1 on pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'hoffmanstmpy',
-  version = '1.0.0',
+  version = '1.0.1',
   packages = find_packages(),
   package_data = {
     "":["*.txt", "*.mat"]
